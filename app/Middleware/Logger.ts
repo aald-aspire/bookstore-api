90db49041dc51e444b6242defab0d06781c77211
ABDULLAH ALDAWOOD <aaldawood@aspire-services.net>
2023-06-22 18:02:06 +0300

    Added logging facilities (no log transports yet) Minor fixes/enhancements

---
+++ app/Middleware/Logger.ts
@@ -0,0 +1,32 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+import Logger from '@ioc:Adonis/Core/Logger'
+
+export default class Logger {
+
+	public async handle (
+	    ctx: HttpContextContract,
+	    next: () => Promise<void>
+  	) {
+  		// TODO: Adjust to rely on config parameters in order to gain more control during runtime
+
+  		Logger.info('%s %s => Request: %o', ctx.request.method(), ctx.request.url(), ctx.request.body())
+
+  		await next()
+
+  		const status = ctx.response.getStatus()
+
+  		if(status !== null) {
+  			switch (Math.floor(status / 100)) {
+  				case 4:
+  					// Logger.warn('%s %s => Response: %o', ctx.request.method(), ctx.request.url(), ctx.response.getHeaders())
+  					Logger.error('%s %s => Response: %o', ctx.request.method(), ctx.request.url(), ctx.response.getBody() ?? {})
+  					break
+  				case 5:
+  					Logger.fatal('%s %s => Response: %o', ctx.request.method(), ctx.request.url(), ctx.response.getBody() ?? {})
+  					break
+  				default:
+  					Logger.info('%s %s => Response: %o', ctx.request.method(), ctx.request.url(), ctx.response.getBody() ?? {})
+  			}
+  		}
+  	}
+}
